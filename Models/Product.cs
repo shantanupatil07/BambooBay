5c4e79059ae94be1a06724d7a0616e816320c6a7
shantanupatil07 <shantanupatil700@gmail.com>
2024-10-07 01:06:23 +0530

    first commit

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace BambooBayEF.Models
+{
+    public class Product
+    {
+
+        public int? ProductId { get; set; }
+
+        public string? ProductName { get; set; }
+
+        public string? ProductDescription { get; set; }
+
+           public float? ProductPrice { get; set; }
+        public int? productQuantity { get; set; }
+
+    }
+}
